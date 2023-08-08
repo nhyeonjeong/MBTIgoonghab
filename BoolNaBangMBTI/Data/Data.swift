95333aecc5f2e4b70a3a9663aeadd7117a8521eb
nhyeonjeong <hjnam5962@gmail.com>
2023-08-08 20:52:12 +0900

    asd

--- BoolNaBangMBTI/Data/Data.swift
+++ BoolNaBangMBTI/Data/Data.swift
@@ -49,63 +49,66 @@ enum MBTItype {
             return ""
         case .INTJ :
             return ""
-        
-        }
-        
-        var howToFriend: String {
-            switch self {
-            case .ESTP :
-                return ""
-            case .ESTJ :
-                return ""
-            case .ESFP :
-                return ""
-            case .ESFJ :
-                return ""
-            case .ENFP :
-                return ""
-            case .ENFJ :
-                return ""
-            case .ENTP :
-                return ""
-            case .ENTJ :
-                return ""
-            case .ISTP :
-                return ""
-            case .ISTJ :
-                return ""
-            case .ISFP :
-                return ""
-            case .ISFJ :
-                return ""
-            case .INFP :
-                return ""
-            case .INFJ :
-                return ""
-            case .INTP:
-                return ""
-            case .INTJ :
-                return ""
             
-            }
+        }
+    }
+    
+    var howToFriend: String {
+        switch self {
+        case .ESTP :
+            return ""
+        case .ESTJ :
+            return ""
+        case .ESFP :
+            return ""
+        case .ESFJ :
+            return ""
+        case .ENFP :
+            return ""
+        case .ENFJ :
+            return ""
+        case .ENTP :
+            return ""
+        case .ENTJ :
+            return ""
+        case .ISTP :
+            return ""
+        case .ISTJ :
+            return ""
+        case .ISFP :
+            return ""
+        case .ISFJ :
+            return ""
+        case .INFP :
+            return ""
+        case .INFJ :
+            return ""
+        case .INTP:
+            return ""
+        case .INTJ :
+            return ""
+            
+        }
+    }
+    
+    
+    struct Profile: Identifiable {
+        var id: UUID = UUID()
+        var name: String
+        var mbti: MBTItype
+        var gender: String
+        
+    }
+    
+    struct Opponent: Identifiable {
+        var id: UUID = UUID()
+        var oppName: String
+        var oppMbti: MBTItype
+    }
+    
+    class OpponentStore {
+        var opponents: [Opponent] = []
+        
     }
 }
 
-struct Profile: Identifiable {
-    var id: UUID = UUID()
-    var name: String
-    var mbti: MBTItype
-    var gender: String
-
-}
-
-struct Opponent: Identifiable {
-    var id: UUID = UUID()
-    var oppName: String
-    var oppMbti: MBTItype
-}
-
-class OpponentStore {
-    var opponents: [Opponent] = []
-    
-}
